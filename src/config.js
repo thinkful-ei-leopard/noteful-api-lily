c1dd658980c26979ca2f67a5127640650417f65a
Lillian Burnside <lillian.burnside@gmail.com>
2020-03-08 22:24:17 -0500

    some fixes?

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:123@localhost/noteful',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:123@localhost/blogful-test'
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:123@localhost/noteful-test'
 };
